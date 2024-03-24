d135307d3717445bcefe4b4c7d748a278bea91d4
Santiago Smith <16252054+sssilvar@users.noreply.github.com>
2024-03-24 16:46:15 +0100

    chore: lesson 1 - hello world

---
+++ examples/00_hello_world.rs
@@ -0,0 +1,11 @@
+/* Hello world app
+
+The first must-do app in any language you start to learn.
+To compile you can use cargo as follows:
+
+cargo run --color=always --package learning-rust --example 00_hello_world
+*/
+fn main() {
+    let who = "World";
+    println!("Hello, {who}!");
+}
